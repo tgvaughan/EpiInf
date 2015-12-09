3ebaeeafa2bb8328c366f4f6fc6ca7afe11e87b3
Tim Vaughan <tgvaughan@gmail.com>
2015-12-10 09:44:12 +1300

    Moved infect event conditioning to rate.
    Now have an allowed infection event rate and a
    forbidden infection event rate.

--- src/epiinf/distribs/SMCTreeDensity.java
+++ src/epiinf/distribs/SMCTreeDensity.java
@@ -210,6 +210,10 @@ public class SMCTreeDensity extends TreeDistribution {
             model.calculatePropensities(particleState);
 
             double infectionProp = model.propensities[EpidemicEvent.INFECTION];
+            double allowedInfectProp = infectionProp
+                    *(1.0 - lineages * (lineages - 1) / particleState.I / (particleState.I + 1));
+            double forbbiddenInfectProp = infectionProp - allowedInfectProp;
+
             double allowedRecovProp, forbiddenRecovProp;
             if (particleState.I > lineages) {
                 allowedRecovProp = model.propensities[EpidemicEvent.RECOVERY];
@@ -218,7 +222,7 @@ public class SMCTreeDensity extends TreeDistribution {
                 allowedRecovProp = 0.0;
                 forbiddenRecovProp = model.propensities[EpidemicEvent.RECOVERY];
             }
-            double allowedEventProp = infectionProp + allowedRecovProp;
+            double allowedEventProp = allowedInfectProp + allowedRecovProp;
 
             // Determine size of time increment
 
@@ -234,7 +238,7 @@ public class SMCTreeDensity extends TreeDistribution {
             double trueDt = Math.min(dt, Math.min(nextModelEventTime, finalTreeEvent.time) - particleState.time);
             conditionalLogP += -trueDt*(model.propensities[EpidemicEvent.PSI_SAMPLE_REMOVE]
                     + model.propensities[EpidemicEvent.PSI_SAMPLE_NOREMOVE]
-                    + forbiddenRecovProp);
+                    + forbbiddenInfectProp + forbiddenRecovProp);
 
             // Increment time
             particleState.time += dt;
@@ -258,16 +262,10 @@ public class SMCTreeDensity extends TreeDistribution {
 
             EpidemicEvent event = new EpidemicEvent();
             event.time = particleState.time;
-            if (allowedEventProp*Randomizer.nextDouble() < infectionProp) {
+            if (allowedEventProp*Randomizer.nextDouble() < allowedInfectProp) {
                 event.type = EpidemicEvent.INFECTION;
             } else
                 event.type = EpidemicEvent.RECOVERY;
-
-
-            // Condition against infection events that produce coalescences not
-            // observed in tree.
-            if (event.type == EpidemicEvent.INFECTION)
-                conditionalLogP += Math.log(1.0 - lineages * (lineages - 1) / particleState.I / (particleState.I + 1));
 
             model.incrementState(particleState, event);
 
